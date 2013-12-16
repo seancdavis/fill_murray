1f90854ed01dd8e46d54283d69885206120e9918
Sean C Davis <scdavis41@gmail.com>
2013-12-16 16:55:19 -0500

    add options to helper

--- lib/fill_murray/view_helpers.rb
+++ lib/fill_murray/view_helpers.rb
@@ -1,16 +1,17 @@
 module FillMurray
   module ViewHelpers
 
-  	def fill_murray(width = nil, height = nil)
-  		if height = nil
-  			if width == nil
+  	def fill_murray(options = {})
+  		if options[:height].blank?
+  			if options[:width].blank?
 	  			width = Random.new.rand(100...1000)
 	  			height = Random.new.rand(100...1000)
 	  		else
 	  			height = width
 	  		end  			
   		end
-    	image_tag "http://www.fillmurray.com/#{width}/#{height}"
+    	# image_tag "http://www.fillmurray.com/#{width}/#{height}"
+    	"<img src=\"http://www.fillmurray.com/#{width}/#{height}\" class=\"#{options[:class]}\" alt=\"#{options[:alt]}\">"
     end
 
   end
