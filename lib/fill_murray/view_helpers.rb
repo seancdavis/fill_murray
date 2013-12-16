d358783d99e801795da307ab23b63360c17b8335
Sean C Davis <scdavis41@gmail.com>
2013-12-16 16:50:00 -0500

    add variables to helper method

--- lib/fill_murray/view_helpers.rb
+++ lib/fill_murray/view_helpers.rb
@@ -1,13 +1,17 @@
 module FillMurray
   module ViewHelpers
 
-  	def say_hi
-  		"hello"
-  	end
-
-    def fill_murray(width, height)
+  	def fill_murray(width = nil, height = nil)
+  		if height = nil
+  			if width == nil
+	  			width = Random.new.rand(100...1000)
+	  			height = Random.new.rand(100...1000)
+	  		else
+	  			height = width
+	  		end  			
+  		end
     	image_tag "http://www.fillmurray.com/#{width}/#{height}"
     end
-    
+
   end
 end
